b4e98cebf3ddedf6dad5f1688094797e938c8fe1
mohammedprojectss <153554903+mohammedprojectss@users.noreply.github.com>
2023-12-12 02:52:50 +0000

    Add files via upload

---
+++ asimg/index.js
@@ -0,0 +1,27 @@
+import logo from './logo.png'
+import pic1 from './pic1.png'
+import pic2 from './pic2.png'
+import pic3 from './pic3.png'
+
+import dmax from './3dmax-logo.png'
+import rivet from './Rivet-Logo.png'
+import autocad from './autocad-logo.png'
+import priv from './primavera-Logo.png'
+import sket from './sketch-logo.webp'
+
+import work1 from './work1.jpg'
+import work2 from './work2.jpg'
+import work3 from './work3.jpg'
+import work4 from './work4.jpg'
+import work5 from './work5.jpg'
+
+import wall from './wall.jpg'
+import wall2 from './wall2.jpg'
+import wall3 from './wall3.jpg'
+
+import vid from './vid.mp4'
+import vid2 from './vid2.mp4'
+import vid3 from './vid3.mp4'
+
+export {logo , pic1 , pic2 , pic3 , work1 , work2 , work3 , work4 , work5 , vid , vid2 , vid3 , dmax , rivet , autocad , priv , sket , wall , wall2 , wall3};
+
